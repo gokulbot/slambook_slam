894755790ddaddab0546057db91815dcc6148399
gsanthosh@monarchtractor.com <gsanthosh@monarchtractor.com>
2024-02-04 19:45:15 +0530

    first commit

---
+++ build/src/CMakeFiles/myslam.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gsanthosh/SLAM/Code/slambook2/ch13/src/backend.cpp" "/home/gsanthosh/SLAM/Code/slambook2/ch13/build/src/CMakeFiles/myslam.dir/backend.cpp.o"
+  "/home/gsanthosh/SLAM/Code/slambook2/ch13/src/camera.cpp" "/home/gsanthosh/SLAM/Code/slambook2/ch13/build/src/CMakeFiles/myslam.dir/camera.cpp.o"
+  "/home/gsanthosh/SLAM/Code/slambook2/ch13/src/config.cpp" "/home/gsanthosh/SLAM/Code/slambook2/ch13/build/src/CMakeFiles/myslam.dir/config.cpp.o"
+  "/home/gsanthosh/SLAM/Code/slambook2/ch13/src/dataset.cpp" "/home/gsanthosh/SLAM/Code/slambook2/ch13/build/src/CMakeFiles/myslam.dir/dataset.cpp.o"
+  "/home/gsanthosh/SLAM/Code/slambook2/ch13/src/feature.cpp" "/home/gsanthosh/SLAM/Code/slambook2/ch13/build/src/CMakeFiles/myslam.dir/feature.cpp.o"
+  "/home/gsanthosh/SLAM/Code/slambook2/ch13/src/frame.cpp" "/home/gsanthosh/SLAM/Code/slambook2/ch13/build/src/CMakeFiles/myslam.dir/frame.cpp.o"
+  "/home/gsanthosh/SLAM/Code/slambook2/ch13/src/frontend.cpp" "/home/gsanthosh/SLAM/Code/slambook2/ch13/build/src/CMakeFiles/myslam.dir/frontend.cpp.o"
+  "/home/gsanthosh/SLAM/Code/slambook2/ch13/src/map.cpp" "/home/gsanthosh/SLAM/Code/slambook2/ch13/build/src/CMakeFiles/myslam.dir/map.cpp.o"
+  "/home/gsanthosh/SLAM/Code/slambook2/ch13/src/mappoint.cpp" "/home/gsanthosh/SLAM/Code/slambook2/ch13/build/src/CMakeFiles/myslam.dir/mappoint.cpp.o"
+  "/home/gsanthosh/SLAM/Code/slambook2/ch13/src/viewer.cpp" "/home/gsanthosh/SLAM/Code/slambook2/ch13/build/src/CMakeFiles/myslam.dir/viewer.cpp.o"
+  "/home/gsanthosh/SLAM/Code/slambook2/ch13/src/visual_odometry.cpp" "/home/gsanthosh/SLAM/Code/slambook2/ch13/build/src/CMakeFiles/myslam.dir/visual_odometry.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GFLAGS_IS_A_DLL=0"
+  "myslam_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/gsanthosh/SLAM/Code/Pangolin/lib/cmake/Pangolin/../../../include"
+  "/usr/include/suitesparse"
+  "../include"
+  "/usr/include/eigen3"
+  "/home/gsanthosh/SLAM/Code/opencv/include"
+  "/home/gsanthosh/SLAM/Code/opencv/include/opencv"
+  "/home/gsanthosh/SLAM/Code/Pangolin/include"
+  "/home/gsanthosh/SLAM/Code/Sophus/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/test/CMakeFiles/test_triangulation.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gsanthosh/SLAM/Code/slambook2/ch13/test/test_triangulation.cpp" "/home/gsanthosh/SLAM/Code/slambook2/ch13/build/test/CMakeFiles/test_triangulation.dir/test_triangulation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GFLAGS_IS_A_DLL=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/gsanthosh/SLAM/Code/Pangolin/lib/cmake/Pangolin/../../../include"
+  "/usr/include/suitesparse"
+  "../include"
+  "/usr/include/eigen3"
+  "/home/gsanthosh/SLAM/Code/opencv/include"
+  "/home/gsanthosh/SLAM/Code/opencv/include/opencv"
+  "/home/gsanthosh/SLAM/Code/Pangolin/include"
+  "/home/gsanthosh/SLAM/Code/Sophus/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/gsanthosh/SLAM/Code/slambook2/ch13/build/src/CMakeFiles/myslam.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
